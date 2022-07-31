1c135a2c7bddb7550920484b89de31ab1338245a
mpater59 <mikolajpater123@gmail.com>
2022-07-31 18:51:44 +0200

    Modified changeStatus.py

--- changeStatus.py
+++ changeStatus.py
@@ -20,7 +20,7 @@ status = sys.argv[2]
 
 if re.search("\d+/\d+/\d+ \d+:\d+:\d+", config_id):
     updateCondition = {"time": config_id}
-elif re.search("^[0-9a-f]$", config_id):
+elif re.search("^[0-9a-f]+$", config_id):
     updateCondition = {'_id': ObjectId(f"{config_id}")}
 else:
     print("First parameter has to be date with time with YYYY/MM/DD HH:MM:SS format or ID configuration in MongoDB!")
