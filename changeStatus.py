63876bdfa2f8b0a5fe3038611eeff14b1d18fc35
mpater59 <mikolajpater123@gmail.com>
2022-07-31 18:50:59 +0200

    Modified changeStatus.py

--- changeStatus.py
+++ changeStatus.py
@@ -2,6 +2,8 @@ import sys
 import pymongo
 import re
 
+from bson import ObjectId
+
 myclient = pymongo.MongoClient("mongodb://192.168.1.21:9000/")
 mydb = myclient["configsdb"]
 mycol = mydb["configurations"]
@@ -16,13 +18,39 @@ if len(sys.argv) != 3:
 config_id = sys.argv[1]
 status = sys.argv[2]
 
-if re.search("^[\d]/[\d]/[\d] [\d]:[\d]:[\d]$", config_id):
+if re.search("\d+/\d+/\d+ \d+:\d+:\d+", config_id):
     updateCondition = {"time": config_id}
-elif re.search("[0-9a-f]", config_id):
-    updateCondition = {"_id": f'ObjectId("{config_id}")'}
+elif re.search("^[0-9a-f]$", config_id):
+    updateCondition = {'_id': ObjectId(f"{config_id}")}
 else:
     print("First parameter has to be date with time with YYYY/MM/DD HH:MM:SS format or ID configuration in MongoDB!")
     exit()
+
+"""
+print("Select status to update:")
+while True:
+    print("1. Verified")
+    print("2. Unverified")
+    print("3. Other")
+    print("4. Exit")
+    choice = input("Enter choice: ")
+    choice = choice.strip()
+
+    if choice == 1:
+        status = "verified"
+        break
+    elif choice == 2:
+        status = "unverified"
+        break
+    elif choice == 3:
+        status = input("Enter status: ")
+        break
+    elif choice == 4:
+        exit()
+    else:
+        print("Entered wrong number!")
+"""
+
 newValues = {"$set": {"status": status}}
-dbUpdate = mycol.update_many(updateCondition, newValues)
+dbUpdate = mycol.update_one(updateCondition, newValues)
 print(dbUpdate.raw_result)
