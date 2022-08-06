0e171b2904d5e7d1731d5b8ffd790cbdf2198ab1
mpater59 <mikolajpater123@gmail.com>
2022-08-06 15:40:50 +0200

    Modified update.py

--- update.py
+++ update.py
@@ -46,7 +46,7 @@ for counter, device in enumerate(devices):
 
 
 configurationList = {}
-configurationList["time"] = datetime.datetime.now().isoformat()
+configurationList["time"] = datetime.datetime.now().strptime("%Y-%m-%dT%H:%M:%S.%fZ")
 configurationList["status"] = "unverified"
 configurationList["active"] = True
 configurationList["devices"] = {}
