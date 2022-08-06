b499bdb59e1ab6a264f0cc5257bef1d28687aeaa
mpater59 <mikolajpater123@gmail.com>
2022-08-06 18:22:25 +0200

    Modified changeStatus.py

--- changeStatus.py
+++ changeStatus.py
@@ -8,49 +8,49 @@ myclient = pymongo.MongoClient("mongodb://192.168.1.21:9000/")
 mydb = myclient["configsdb"]
 mycol = mydb["configurations"]
 
-for arg in sys.argv:
-    print(arg)
 
-if len(sys.argv) != 3:
-    print("Enter two parameters!")
-    exit()
-
-config_id = sys.argv[1]
-status = sys.argv[2]
-
-if re.search("\d+/\d+/\d+ \d+:\d+:\d+", config_id):
-    updateCondition = {"time": config_id}
-elif re.search("^[0-9a-f]{24}$", config_id):
-    updateCondition = {'_id': ObjectId(f"{config_id}")}
-else:
-    print("First parameter has to be date with time with YYYY/MM/DD HH:MM:SS format or ID configuration in MongoDB!")
-    exit()
-
-"""
-print("Select status to update:")
-while True:
-    print("1. Verified")
-    print("2. Unverified")
-    print("3. Other")
-    print("4. Exit")
-    choice = input("Enter choice: ")
-    choice = choice.strip()
-
-    if choice == 1:
-        status = "verified"
-        break
-    elif choice == 2:
-        status = "unverified"
-        break
-    elif choice == 3:
-        status = input("Enter status: ")
-        break
-    elif choice == 4:
-        exit()
+def changeStatus(change_type, status=None, config_id=None):
+    if change_type == "active":
+        if status is None:
+            status = True
+        if config_id is None:
+            config_id = str(mycol.find({}, {"_id": 1}).sort("date", -1)[0].get("_id"))
+        if re.search("\d+-\d+-\d+T\d+:\d+:\d+.\d+\+\d+:\d+", config_id):
+            update_condition = {"time": config_id}
+        elif re.search("^[0-9a-f]{24}$", config_id):
+            update_condition = {'_id': ObjectId(f"{config_id}")}
+        else:
+            print("Entered wrong format of third parameter!")
+    elif change_type == "status":
+        if status is None:
+            status = "verified"
+        if config_id is None:
+            config_id = str(mycol.find({}, {"_id": 1}).sort("date", -1)[0].get("_id"))
+        if re.search("\d+-\d+-\d+T\d+:\d+:\d+.\d+\+\d+:\d+", config_id):
+            update_condition = {"time": config_id}
+        elif re.search("^[0-9a-f]{24}$", config_id):
+            update_condition = {'_id': ObjectId(f"{config_id}")}
+        else:
+            print("Entered wrong format of third parameter!")
     else:
-        print("Entered wrong number!")
-"""
+        print("Entered wrong first parameter!")
+        exit()
+    newValues = {"$set": {"status": status}}
+    dbUpdate = mycol.update_one(update_condition, newValues)
+    print(dbUpdate.raw_result)
 
-newValues = {"$set": {"status": status}}
-dbUpdate = mycol.update_one(updateCondition, newValues)
-print(dbUpdate.raw_result)
+
+if len(sys.argv) > 1:
+    change_type = sys.argv[1]
+else:
+    print("Enter first parameter!")
+    exit()
+if len(sys.argv) > 2:
+    status = sys.argv[2]
+else:
+    status = None
+if len(sys.argv) > 3:
+    config_id = sys.argv[3]
+else:
+    config_id = None
+changeStatus(change_type, status, config_id)
