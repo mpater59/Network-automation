4601a310fe25da93b7f1ecca1df679534ea0dd4f
mpater59 <mikolajpater123@gmail.com>
2022-10-23 16:10:57 +0200

    Added other.py

---
+++ other.py
@@ -0,0 +1,35 @@
+def compare_list(list1, list2):
+    list1.sort()
+    list2.sort()
+    if list1 == list2:
+        return True
+    else:
+        return False
+
+
+def key_exists(dictionary, *keys):
+    if dictionary is None:
+        return False
+    elif not isinstance(dictionary, dict):
+        return False
+    for key in keys:
+        try:
+            dictionary = dictionary[key]
+            if dictionary is None or dictionary == "":
+                return False
+        except KeyError:
+            return False
+    return True
+
+
+def check_if_exists(var_item, var_list):
+    if var_list is None:
+        return False
+    elif not isinstance(var_list, list):
+        return False
+    for item in var_list:
+        if item == var_item:
+            return True
+        else:
+            continue
+    return False
