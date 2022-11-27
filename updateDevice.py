a5cf2a91bb8fd66cefa52fbff73603428411cc5d
mpater59 <mikolajpater123@gmail.com>
2022-11-27 14:02:04 +0100

    Modified updateDevice.py

--- updateDevice.py
+++ updateDevice.py
@@ -84,8 +84,8 @@ def update_devices(site, devices, status=None, soft_update=True, expand=True):
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-st", "--site", dest="site", help="Name of site")
-parser.add_argument("-df", "--devices", dest="devices",
-                    help="Path to .yaml file with new devices")
+parser.add_argument("-d", "--devices", dest="devices",
+                    help="Name of devices, separate with ',' (default parameter will set status for all devices in selected site)")
 parser.add_argument("-t", "--status_text", dest="status_text", default=None,
                     help="Text status that will be set for this update in DB")
 parser.add_argument("-su", "--soft_update", dest="soft_update", default=False, action='store_true',
